10c5680ca9f4b83e8c8a1002059496fa14e70ae7
Amber Hasan <Amber.Hasan@Geminisolutions.com>
2026-06-16 11:40:47 +0530

    Fix CORS configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app=express();
 app.use(cors({
-    origin: 'http://localhost:5173' || 'http://localhost:5174',
+    origin: process.env.CORS_ORIGIN,
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
