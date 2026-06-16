725a05555a68646616215de1f118aab99c549a0e
Amber Hasan <Amber.Hasan@Geminisolutions.com>
2026-06-16 14:25:51 +0530

    Update multer middleware

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,17 +1,21 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
 
-const storage=multer.diskStorage({
-    destination: function(req , file , cb){
-        cb(null , "./public/temp") //sari files yaha hongi
+
+const uploadDir = path.resolve("public/temp");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
     },
-    filename: function (req ,file ,cb){
-        console.log(file);
-        cb(null , file.originalname)
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`);
     }
-})
+});
 
-
-export const upload= multer({
-     storage ,
-})
-
+export const upload = multer({ storage });
